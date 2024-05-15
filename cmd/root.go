a484e5cb91f4e5c3c0c028c83cd64df066341740
twsnmp <ymi@twise.co.jp>
2024-05-15 15:47:20 +0900

    add global flags

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,20 +24,15 @@ import (
 )
 
 var cfgFile string
+var dataStore string
+var timeRange string
+var filter string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "twsla",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Simple Log Analyzer",
+	Long:  `Simple Log Analyzer by TWSNMP`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -51,16 +46,11 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.twsla.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&dataStore, "datastore", "d", "./twsla.db", "Bblot log db")
+	rootCmd.PersistentFlags().StringVarP(&timeRange, "timeRange", "t", "", "Time range")
+	rootCmd.PersistentFlags().StringVarP(&filter, "filter", "f", "", "Filter")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
